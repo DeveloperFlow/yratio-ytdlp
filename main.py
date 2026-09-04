dd594f004c8ac532d5cb5864528cfb2649d3fad3
Chigoziem Chidiebube <chigoziemchidiebube@gmail.com>
2026-09-04 16:25:36 +0100

    Added code to test ip

--- main.py
+++ main.py
@@ -5,10 +5,44 @@ app = FastAPI()
 
 @app.get("/extract")
 def extract_url(url: str):
-    ydl_opts = {'extract_flat': True}
+    ydl_opts = {
+        'extract_flat': True
+    }
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
             return {"status": "success", "url": info.get("url")}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/test")
+def test(url: str):
+    HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Host": "www.instagram.com",
+    "Pragma": "no-cache",
+    "Sec-Ch-Ua": '"Chromium";v="152", "Not?A_Brand";v="24", "Google Chrome";v="152"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36",
+    "X-Forwarded-For": "105.112.39.72",
+    "X-Forwarded-Proto": "https",
+    "X-Real-Ip": "105.112.39.72"
+    }
+    try:
+        response = requests.get(url, headers=HEADERS, timeout=10)
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+            "body": response.text
+        }
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
